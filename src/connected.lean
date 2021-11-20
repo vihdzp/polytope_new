9d171d7223daf1e25489fa8e615865ba76fe3db5
ocornoc <ocornoc@protonmail.com>
2021-11-20 12:49:21 -0800

    golfing and styling

--- src/connected.lean
+++ src/connected.lean
@@ -1,54 +1,49 @@
 import .abstract
 
+variables {α : Type*} [bounded_graded α] {a : α}
+
 /-- Connectivity is reflexive. -/
-theorem connected.refl {α : Type*} [bounded_graded α] : ∀ {a : α}, proper a → connected a a := 
-λ a pa, (connected.start a) pa
+@[refl]
+theorem connected.refl : proper a → connected a a := 
+connected.start a
+
+variable {b : α}
 
 /-- Comparable proper elements are connected. -/
-theorem comp_to_connected {α : Type*} [bounded_graded α] : ∀ {a b : α}, proper a → proper b → comparable a b → connected a b :=
-λ a b pa pb hab, (connected.next a a b) (connected.refl pa) pb hab
+theorem comp_to_connected (ha : proper a) : proper b → comparable a b → connected a b :=
+(connected.next a a b) (connected.refl ha)
 
 /-- If `a` and `b` are comparable proper elements, and `b` and `c` are connected, 
     then `a` and `c` are connected. -/
-lemma connected.append {α : Type*} [bounded_graded α] : ∀ {a b c : α}, proper a → comparable a b → connected b c → connected a c :=
+lemma connected.append {c : α} (ha : proper a) (hab : comparable a b) (hbc : connected b c) :
+  connected a c :=
 begin
-  intros _ _ _ pa hbc cbc,
-  induction cbc with _ px _ y z _ pz hyz h, {
-    exact comp_to_connected pa px hbc,
-  },
-  exact (connected.next a y z) (h hbc) pz hyz,
+  induction hbc with _ hx _ y z _ hz hyz h,
+    { exact comp_to_connected ha hx hab },
+    { exact (connected.next a y z) (h hab) hz hyz }
 end
 
 /-- Connectedness is symmetric. -/
-theorem connected.symm {α : Type*} [bounded_graded α] : ∀ {a b : α}, connected a b → connected b a := 
+@[symm]
+theorem connected.symm (hab : connected a b) : connected b a :=
 begin
-  intros _ _ hab,
-  induction hab with a pa _ _ _ _ pz hyz hyx, {
-    exact connected.start a pa,
-  },
-  exact connected.append pz hyz.symm hyx,
+  induction hab with a pa _ _ _ _ pz hyz hyx,
+    { exact connected.start a pa },
+    { exact connected.append pz hyz.symm hyx }
 end
 
 /-- If `a` and `b` are comparable, then `a` is proper. -/
-lemma connected.proper {α : Type*} [bounded_graded α] : ∀ {a b : α}, connected a b → proper a :=
-begin
-  intros _ _ hab,
-  induction hab with _ h _ _ _ _ _ _ h, {
-    exact h,
-  },
-  exact h,
-end
+lemma connected.proper (hab : connected a b) : proper a :=
+by induction hab with _ h _ _ _ _ _ _ h; exact h
 
 /-- If `a` and `b` are comparable, then `b` is proper. -/
-lemma connected.proper' {α : Type*} [bounded_graded α] : ∀ {a b : α}, connected a b → proper b :=
-λ _ _ hab, hab.symm.proper
+lemma connected.proper' (hab : connected a b) : proper b :=
+hab.symm.proper
 
 /-- Connectedness is transitive. -/
-theorem connected.trans {α : Type*} [bounded_graded α] : ∀ {a b c : α}, connected a b → connected b c → connected a c :=
+theorem connected.trans {c : α} (hab : connected a b) (hbc : connected b c) : connected a c :=
 begin
-  intros a b c hab hbc,
-  induction hab with _ _ _ _ _ hxy _ hyz h, {
-    exact hbc,
-  },
-  exact h (connected.append hxy.proper' hyz hbc),
+  induction hab with _ _ _ _ _ hxy _ hyz h,
+    { exact hbc },
+    { exact h (connected.append hxy.proper' hyz hbc) }
 end
